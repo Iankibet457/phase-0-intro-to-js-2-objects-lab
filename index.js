63ae4d0360b7e1c10bca647cb507193cbc2b59da
ian mosonik <ian.mosonik@student.moringaschool.com>
2024-09-19 13:16:20 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+let employee = {
+    name:"ian",
+    streetAddress:"langata"
+}
+function updateEmployeeWithKeyAndValue(obj,key,value){
+    obj = {...employee}
+    obj[key]=value
+    return obj
+    
+
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value
+    return employee
+
+}
+function deleteFromEmployeeByKey(obj,key){
+    obj={...employee}
+    delete obj[key]
+    return obj
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee,key){
+    delete employee[key]
+    return employee
+
+}
